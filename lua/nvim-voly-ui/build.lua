798c0ed14464671f0aa4f5c8d4f7fad34053c184
Will Vachon <will@voly.co.uk>
2024-08-12 16:44:49 +0100

    test command

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -1,15 +1,7 @@
 local Module = {}
 
 function Module.dev_build()
-    vim.fn.jobstart('ls', { on_stdout = function(id, data, name)
-            vim.print("id")
-            vim.print(id)
-            vim.print("data")
-            vim.print(data)
-            vim.print("name")
-            vim.print(name)
-        end
-    })
+    vim.fn.jobstart("ls -al /home/will/")
 end
 
 return Module
