b79dfd79e22187463d8977ba42d361ba9a4dc52c
Will Vachon <will@voly.co.uk>
2024-08-12 16:28:43 +0100

    Update command testing

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -2,8 +2,11 @@ local Module = {}
 
 function Module.dev_build()
     vim.fn.jobstart('ls', { on_stdout = function(id, data, name)
+            vim.print("id")
             vim.print(id)
+            vim.print("data")
             vim.print(data)
+            vim.print("name")
             vim.print(name)
         end
     })
