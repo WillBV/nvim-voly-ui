4e124a0062f485fb5b56adbcafcb55de2c30fef9
Will Vachon <will@voly.co.uk>
2024-08-12 17:54:19 +0100

    test sync function

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -4,13 +4,13 @@ local Module = {}
 
 
 function Module.dev_build()
-    vim.fn.jobstart("ls -al /home/will/",{
-        on_stdout = function(j, d, e)
-            if vim.inspect(d) then
-                print(vim.inspect(d))
-            end
-        end,
-    })
+ --   vim.fn.jobstart("ls -al /home/will/",{
+ --       on_stdout = function(j, d, e)
+ --           print(vim.inspect(d))
+ --       end,
+ --   })
+    local output = vim.fn.system("ls -al /home/will/")
+    print(output)
 end
 
 return Module
