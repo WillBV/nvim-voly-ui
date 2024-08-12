52e896711ccd8183c950790210b18c93bd0b306f
Will Vachon <will@voly.co.uk>
2024-08-12 17:07:02 +0100

    refactor print stmnt

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -1,11 +1,13 @@
 local Module = {}
 
 function Module.dev_build()
+    local output = ""
     vim.fn.jobstart("ls -al /home/will/",{
         on_stdout = function(j, d, e)
-            print(vim.inspect(d))
+            output = output .. vim.fn.join(d)
         end,
     })
+    print(output)
 end
 
 return Module
