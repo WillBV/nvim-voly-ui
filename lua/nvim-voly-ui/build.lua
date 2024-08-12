d8dba98a3196e7105c65f163941cab79b1dc6fe7
Will Vachon <will@voly.co.uk>
2024-08-12 17:58:49 +0100

    debug no output

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -4,13 +4,13 @@ local Module = {}
 
 
 function Module.dev_build()
- --   vim.fn.jobstart("ls -al /home/will/",{
- --       on_stdout = function(j, d, e)
- --           print(vim.inspect(d))
- --       end,
- --   })
-    local output = vim.fn.system("sleep 2 && ls -al /home/will/")
-    print(output)
+    vim.fn.jobstart("npm run instantiation-scripts-gen",{
+        on_stdout = function(j, d, e)
+            print("Instantiation Scripts Run")
+        end,
+    })
+ --   local output = vim.fn.system("sleep 2 && ls -al /home/will/")
+ --   print(output)
 end
 
 return Module
