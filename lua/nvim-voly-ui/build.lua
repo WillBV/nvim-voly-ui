f964fe5d236b2cf2412f76339f5be18f1b62792c
Will Vachon <will@voly.co.uk>
2024-08-12 20:28:15 +0100

    debug

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -3,13 +3,13 @@ local Module = {}
 function Module.dev_build()
 	vim.fn.jobstart("npm run instantiation-scripts-gen", {
 		on_stdout = function(j, d, e)
-			-- print(vim.inspect(d))
+			print(vim.inspect(d))
 			print("instantiation scripts run")
 		end,
 	})
 	vim.fn.jobstart("npm run build-dev", {
 		on_stdout = function(j, d, e)
-			-- print(vim.inspect(d))
+			print(vim.inspect(d))
 			print("Build Complete")
 		end,
 	})
