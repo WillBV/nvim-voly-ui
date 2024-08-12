2adc53664bb828b216dfdb2e301e3d61f3e16765
Will Vachon <will@voly.co.uk>
2024-08-12 17:38:25 +0100

    debug

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -1,13 +1,13 @@
 local Module = {}
 
 function Module.dev_build()
-    local output = ""
+    local output = "Start: "
     vim.fn.jobstart("ls -al /home/will/",{
         on_stdout = function(j, d, e)
             output = output .. vim.fn.join(d)
         end,
     })
-    print(output)
+    vim.print(output)
 end
 
 return Module
