7ed00c34e3c47e938402eae93ecd60235dbcc77e
Will Vachon <will@voly.co.uk>
2024-08-12 16:07:09 +0100

    Update for testing jobs

--- lua/nvim-voly-ui/build.lua
+++ lua/nvim-voly-ui/build.lua
@@ -2,12 +2,13 @@ local Module = {}
 
 function Module.dev_build()
     vim.fn.jobstart(
-        'echo hello',
+        'ls',
         {
             -- cwd = '~/projects/voly-ui/ui-invoices/',
             -- on_exit = some_function,
             -- on_stdout = some_other_function,
             -- on_stderr = some_third_function
+            on_stdout = print_stdout
         }
     )
     -- os.execute("npm run instantiation-scripts-gen")
